b7083e3e494fb405f823a7287a0d8229cca75d2d
Marin836 <ky436005@gmail.com>
2026-09-01 11:14:40 +0700

    ทำตามใน class เสร็จ

--- Assets/Scripts/Workspace/Assignment/StudentSolution/LCT01SyntaxClass.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/LCT01SyntaxClass.cs
@@ -5,7 +5,19 @@ namespace Assignment.StudentSolution.LCT01
 {
     public class Car
     {
+        public string Name;
+        public string Speed;
+        public string Color;
 
+        public void Move()
+        {
+            Debug.Log("Car is moving");  
+        }
+
+        public void turn()
+        {
+            Debug.Log("Car is turngin");    
+        }
     }
 
     public class LCT01SyntaxClass
@@ -13,7 +25,17 @@ namespace Assignment.StudentSolution.LCT01
         public void Start()
         {
             // Student code start HERE ...
+            
+            Car car1 = new Car();
+            car1.Name = "Lamborgini";
+            Debug.Log(car1.Name);
+            car1.Move();
+            car1.turn();
 
+
+            Car car2 = new();
+            car2.Name = "TOYOTA";
+            Debug.Log(car2.Name);
             // Student code ends HERE 
         }
     }
--- Assets/Scripts/Workspace/Assignment/StudentSolution/LCT03Inheritance.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/LCT03Inheritance.cs
@@ -14,15 +14,25 @@ namespace Assignment.StudentSolution.LCT03
     }
 
     // class Dog inherits from Animal
-    public class Dog
+    public class Dog : Animal
     {
-
+        public void Walk()
+        {
+            Debug.Log($"{name} is Walking");
+        }
     }
 
     // class Bird inherits from Animal
-    public class Bird
+    public class Bird : Animal 
     {
-
+        public void Walk()
+        {
+            Debug.Log($"{name} is Walking");
+        }
+        public void Fly()
+        {
+            Debug.Log($"{name} is Flying");
+        }
     }
 
     public class LCT03Inheritance
@@ -35,13 +45,16 @@ namespace Assignment.StudentSolution.LCT03
             // + เรียกใช้ method MakeSound() ของ dog
             // + เรียกใช้ method Walk() ของ dog
             Dog dog = new Dog();
-
+            dog.name = "Buddy";
+            dog.MakeSound();
 
             // 2. สร้าง instance ของ class Bird โดยกำหนดชื่อตัวแปรว่า bird
             // + กำหนดชื่อ (name) ว่า "Twitty"
             // + เรียกใช้ method MakeSound() ของ bird
             // + เรียกใช้ method Fly() ของ bird
             Bird bird = new();
+            bird.name = "Twitty";
+            bird.MakeSound();
 
         }
     }
